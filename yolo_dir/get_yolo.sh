d47a495a550ef752e7913f7b9fd5fec6ba56fa41
Aditya <adityadeshpande2010@gmail.com>
2019-09-08 19:43:17 -0400

    Update with YOLO model in OpenCV

---
+++ yolo_dir/get_yolo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://pjreddie.com/media/files/yolov3.weights
+wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg
+wget https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names
