74d72c97c186664dc4acb453ebd37c799e0ccba9
adipandas <adityadeshpande2010@gmail.com>
2026-07-16 21:37:48 -0400

    Rebuild Sphinx HTML documentation

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.0.2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
