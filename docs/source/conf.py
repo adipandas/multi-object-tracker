e49604ae6ccd2bfdcbeac4077d642db02ef0fd72
adipandas <adityadeshpande2010@gmail.com>
2026-07-16 22:30:53 -0400

    Bump version to 0.1.1 and rebuild docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Aditya M. Deshpande'
 author = 'Aditya M. Deshpande'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
