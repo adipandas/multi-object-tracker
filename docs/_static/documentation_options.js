a12463b15100a0f43b9f6dc9e3ea148619b68ec7
adipandas <adityadeshpande2010@gmail.com>
2026-07-16 21:46:42 -0400

    Bump version to 0.1.0 and rebuild docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.2',
+    VERSION: '0.1.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Aditya M. Deshpande'
 author = 'Aditya M. Deshpande'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
